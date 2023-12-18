5965daed3da7e82a62d551ea6b9dc8f8b2a62dd7
prithwirajpm <wt.prithwiraj@gmail.com>
2023-12-18 12:16:03 +0530

    using JSON data fake API

--- src/Component/BlongUpdates.jsx
+++ src/Component/BlongUpdates.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function BlongUpdates() {
+  const [allData, setAllData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      console.log(response.data);
+      setAllData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="h-screen xl:px-24 xl:mt-0 xl:mb-10 mt-[20px]">
       <div className="flex justify-center items-center mb-[1.125rem]">
@@ -20,117 +36,49 @@ function BlongUpdates() {
       </div>
       <div className="mt-8">
         <div className="xl:flex justify-around">
-          <div className="xl:w-3/12 my-6 mx-6">
-            <div>
-              <img
-                src="https://www.ombrulla.com/_astro/blog-1.311eedd5_wt4eW.webp"
-                alt="blog"
-                srcset=""
-                style={{ width: "350px", height: "196px" }}
-                className="rounded-lg "
-              />
-            </div>
-            <div className="mt-3">
-              <div>
-                <h4 className="font-medium text-[20px]">
-                  Privacy and security concerns with Artificial Intelligence
-                </h4>
-              </div>
-              <div className="mt-3">
-                <p style={{ color: "grey" }}>
-                  Privacy and security are significant concerns when it comes to
-                  AI. AI systems often require access to large amounts of
-                  personal data, which can be ...
-                </p>
-              </div>
-            </div>
-            <div className="flex mt-4">
-              <img
-                src="https://www.ombrulla.com/_astro/zara-elizabeth.f546c493_wXvHD.webp"
-                alt="zara"
-                srcset=""
-                style={{ width: "40px", height: "40px" }}
-              />
-              <div className="ms-3">
-                <h1 className="font-medium">Zara Elizabeth</h1>
-                <p>Mar 16, 2020 · 6 min read</p>
-              </div>
-            </div>
-          </div>
-          <div className="xl:w-3/12 my-6 mx-6">
-            <div>
-              <img
-                src="https://www.ombrulla.com/_astro/blog-2.c3cc33af_ZkLCIU.webp"
-                alt="blog"
-                srcset=""
-                style={{ width: "350px", height: "196px" }}
-                className="rounded-lg "
-              />
-            </div>
-            <div className="mt-3">
-              <div>
-                <h4 className="font-medium text-[20px]">
-                  The impact of AI on jobs and the workforce
-                </h4>
-              </div>
-              <div className="mt-3">
-                <p style={{ color: "grey" }}>
-                  The impact of AI on jobs and the workforce is a topic of much
-                  debate and speculation. While some argue that AI will create
-                  new job opportunities...
-                </p>
-              </div>
-            </div>
-            <div className="flex mt-4">
-              <img
-                src="https://www.ombrulla.com/_astro/zara-elizabeth.f546c493_wXvHD.webp"
-                alt="zara"
-                srcset=""
-                style={{ width: "40px", height: "40px" }}
-              />
-              <div className="ms-3">
-                <h1 className="font-medium">Zara Elizabeth</h1>
-                <p>Mar 10, 2020 · 4 min read</p>
-              </div>
-            </div>
-          </div>
-          <div className="xl:w-3/12 my-6 mx-6">
-            <div className="w-fit">
-              <img
-                src="https://www.ombrulla.com/_astro/blog-3.fee62469_Z3YRpV.webp"
-                alt="blog"
-                srcset=""
-                style={{ width: "350px", height: "196px" }}
-                className="rounded-lg "
-              />
-            </div>
-            <div className="mt-3">
-              <div>
-                <h4 className="font-medium text-[20px]">
-                  Bias and fairness in Artificial Intelligence
-                </h4>
-              </div>
-              <div className="mt-3">
-                <p style={{ color: "grey" }}>
-                  Bias and fairness are important considerations in AI, as they
-                  can significantly impact the accuracy and effectiveness of AI
-                  systems, as well as the social and ethical of their use...
-                </p>
-              </div>
-            </div>
-            <div className="flex mt-4">
-              <img
-                src="https://www.ombrulla.com/_astro/zara-elizabeth.f546c493_wXvHD.webp"
-                alt="zara"
-                srcset=""
-                style={{ width: "40px", height: "40px" }}
-              />
-              <div className="ms-3">
-                <h1 className="font-medium">Zara Elizabeth</h1>
-                <p>Feb 12, 2020 · 11 min read</p>
-              </div>
-            </div>
-          </div>
+          {allData?.length > 0 ? (
+            allData
+              .filter((product, index) => index < 3)
+              .map((product, index) => (
+                <div className="xl:w-3/12 my-6 mx-6" key={index}>
+                  <div>
+                    <img
+                      src={product.image}
+                      alt="blog"
+                      srcset=""
+                      style={{ width: "350px", height: "196px" }}
+                      className="rounded-lg "
+                    />
+                  </div>
+                  <div className="mt-3">
+                    <div>
+                      <h4 className="font-medium text-[20px]">
+                        {product.title}
+                      </h4>
+                    </div>
+                    <div className="mt-3">
+                      <p style={{ color: "grey" }}>
+                        {product.description.slice(0, 125)}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex mt-4">
+                    <img
+                      src="https://www.ombrulla.com/_astro/zara-elizabeth.f546c493_wXvHD.webp"
+                      alt="zara"
+                      srcset=""
+                      style={{ width: "40px", height: "40px" }}
+                    />
+                    <div className="ms-3">
+                      <h1 className="font-medium">Zara Elizabeth</h1>
+                      <p>Mar 16, 2020 · 6 min read</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+          ) : (
+            <div>Nothing</div>
+          )}
         </div>
       </div>
     </div>
